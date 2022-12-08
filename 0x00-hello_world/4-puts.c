98f9a083e22d896ef4476cddd1657c4434229853
Daniel930603 <daniel.degebas.2@gmail.com>
2022-12-08 04:24:22 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
+ *  main - main block
  *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
+ *  Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
