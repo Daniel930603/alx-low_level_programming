e7b3738c607ec6ae6b0666bca25927451095b0b5
Daniel930603 <daniel.degebas.2@gmail.com>
2022-12-13 05:06:21 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+_putchar('\n');
+}
